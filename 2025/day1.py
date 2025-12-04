88e7a9d216c1db6ff09ccdcd1313949ff95fcd30
Will Flores <waflores956@gmail.com>
2025-12-03 21:54:46 -0500

    Adding a file and python code to read this file in.

--- 2025/day1.py
+++ 2025/day1.py
@@ -2,10 +2,29 @@
 Day 1 - 2025
 """
 
+import re
+
+from python.runfiles import Runfiles
+
+INSTRUCTION_RE = re.compile(r"(L|R)(\d+)")
+
+
+def part1() -> None:
+    """We start at 50 and take in a sequence of + and - to reach a final number."""
+    r = Runfiles.Create()
+
+    # starting_point = 50
+    with open(r.Rlocation("_main/2025/day1_part1_input.txt"), encoding="utf8") as fh:
+        instructions = fh.read().strip().splitlines()
+
+    for instruction in instructions:
+        direction, amount = INSTRUCTION_RE.match(instruction).groups()
+        print(f"Direction: {direction}, Amount: {amount}")
+
 
 def main() -> None:
     """Entry point"""
-    print("I am running from Bazel!")
+    part1()
 
 
 if __name__ == "__main__":
